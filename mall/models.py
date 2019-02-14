e4cf94c05e99b01824d0edb3c19108ed64e7d5c3
taojy123 <taojy123@163.com>
2019-02-14 17:21:43 +0800

    modify mall models

--- mall/models.py
+++ mall/models.py
@@ -9,13 +9,29 @@ import logging
 logger = logging.getLogger('apps')
 
 
+class Banner(Model):
+
+    shop = models.ForeignKey('shop.Shop')
+    pic = models.CharField(max_length=500, blank=True)
+    position = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.pic
+
+
 class Product(Model):
 
     shop = models.ForeignKey('shop.Shop')
+
     name = models.CharField(max_length=255, blank=True)
-    description = models.TextField(blank=True)
-    pic = models.CharField(max_length=500, blank=True, help_text='主图')
     price = models.FloatField(default=100)
+    pic = models.CharField(max_length=500, blank=True, help_text='主图')
+
+    description = models.TextField(blank=True)
+    description_pic = models.CharField(max_length=500, blank=True, help_text='详情图')
+
+    is_hot = models.CharField(max_length=500, blank=True, help_text='是否热卖')
+    hot_pic = models.CharField(max_length=500, blank=True, help_text='热卖图')
 
     def __str__(self):
         return self.name
