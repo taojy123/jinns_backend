5b136adb9660f6ea1c61fa63de2932d9a078beb6
taojy123 <taojy123@163.com>
2019-02-14 17:34:10 +0800

    modify mall admins

--- mall/admin.py
+++ mall/admin.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 from mall import models
 
 
+@admin.register(models.Banner)
+class BannerAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'shop', 'pic', 'position']
+    raw_id_fields = ['shop']
+    list_filter = ['shop']
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
 
-    list_display = ['id', 'shop', 'name', 'price']
+    list_display = ['id', 'shop', 'name', 'price', 'is_hot']
     raw_id_fields = ['shop']
     search_fields = ['name']
     list_filter = ['shop']
