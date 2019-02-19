9242497260afa012b24f3b2ec9c7673e23df56aa
taojy123 <taojy123@163.com>
2019-02-19 15:41:25 +0800

    add product filter

--- mall/views.py
+++ mall/views.py
@@ -1,8 +1,10 @@
 
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+from django_filters import rest_framework as filters, STRICTNESS
 from rest_framework import viewsets, mixins, generics, exceptions, response
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
 
 from mall.models import Product, Banner
 from mall.serializers import ProductSerializer, BannerSerializer
@@ -21,8 +23,22 @@ class BannerViewSet(viewsets.ModelViewSet):
         return Banner.objects.filter(shop=self.request.shop)
 
 
+class ProductFilter(filters.FilterSet):
+
+    order_by = filters.ProductingFilter(fields=['id', 'is_hot', 'name'])
+
+    class Meta:
+        strict = STRICTNESS.RETURN_NO_RESULTS
+        model = Product
+        fields = {
+            'id': ['exact', 'in'],
+            'is_hot': ['exact'],
+        }
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
+    filter_class = ProductFilter
 
     def get_queryset(self):
         return Product.objects.filter(shop=self.request.shop)
